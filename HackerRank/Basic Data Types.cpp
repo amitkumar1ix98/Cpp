3208934080b217f949fab8477eb821175bdf0343
AMIT KUMAR <93512684+amitkumar1ix98@users.noreply.github.com>
2021-11-04 14:40:45 +0530

    Create Basic Data Types.cpp

---
+++ HackerRank/Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a; long b; char c; float f; double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<a<<endl<<b<<endl;
+    cout<<c<<endl;
+    printf("%.3f",f);
+    cout<<endl;
+    printf("%.9f",d);
+    
+    return 0;
+}
